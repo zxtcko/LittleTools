0f8ac5e82ff0a3af3da6690bfba3dbd107666903
Chen Yang <foureveryoung@163.com>
2016-07-26 14:50:20 +0800

    add automation script

---
+++ Instruments/Automation Scripts/Basic Script.js
@@ -0,0 +1,9 @@
+
+var target = UIATarget.localTarget();
+var app = target.frontMostApp();
+target.logEtarget.frontMostApp().mainWindow().scrollViews()[0].images()["yindao2"].scrollToVisible();
+           target.frontMostApp().mainWindow().scrollViews()[0].images()["yindao2"].scrollToVisible();
+           target.frontMostApp().mainWindow().scrollViews()[0].images()["yindao3"].scrollToVisible();
+           target.frontMostApp().mainWindow().scrollViews()[0].buttons()["tiaoguo"].tap();
+           lementTree();
+
